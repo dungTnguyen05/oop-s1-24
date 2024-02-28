a6c5fd555c3d1d341669a9f862358ef00279f0f1
dungTnguyen05 <tridung.190705@gmail.com>
2024-02-28 18:55:31 +1030

    function-3-4.cpp

---
+++ function-3-4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void print_pass_fail(char grade) {
+    switch(grade) {
+        case 'A':
+            cout << "Pass";
+            break;
+
+        case 'B':
+            cout << "Pass";
+            break;
+
+        case 'C':
+            cout << "Pass";
+            break;
+
+        case 'D':
+            cout << "Fail";
+            break;
+        
+        case 'E':
+            cout << "Fail";
+            break;
+
+        default:
+            cout << "Nothing";
+    }
+}

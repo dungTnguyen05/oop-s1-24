9e2afdb4e674f9cf95bbe6f3e6213295404513e1
dungTnguyen05 <tridung.190705@gmail.com>
2024-02-29 11:28:16 +1030

    function-1-4.cpp

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,9 +1,15 @@
 int sum_two_arrays(int array[], int secondarray[], int n) {
-    int sum = 0;
-
-    for (int i = 0; i < n; i++) {
-        sum += array[i] + secondarray[i];
+    if (n < 1) {
+        return 0;
     }
 
-    return sum;
+    else {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += array[i] + secondarray[i];
+        }
+
+        return sum;
+    }
 }

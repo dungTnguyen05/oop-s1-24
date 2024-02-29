196219984d250694cbc2badc001c9057efafd952
dungTnguyen05 <tridung.190705@gmail.com>
2024-02-29 23:41:28 +1030

    main-2-4.cpp

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -4,5 +4,15 @@ using namespace std;
 extern bool is_ascending(int array[], int n);
 
 int main() {
-    int a[6];
+    int a[6] = {1, 1, 2, 2, 3, 3};
+    int x = 6;
+    cout << is_ascending(a, x) << endl;
+
+    int b[5] = {1, 1, 7, 9, 1};
+    int y = 5;
+    cout << is_ascending(b, y) << endl;
+
+    int c[0] = {};
+    int z = 0;
+    cout << is_ascending(c, z) << endl;
 }

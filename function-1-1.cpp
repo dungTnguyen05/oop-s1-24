99017deda5c39ca0475e4c95a49a28727ab0351a
dungTnguyen05 <tridung.190705@gmail.com>
2024-02-29 10:53:59 +1030

    function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,9 +1,15 @@
 int array_sum(int array[], int n) {
-    int sum = 0;
-
-    for (int i = 0; i < n; i++) {
-        sum += array[i];
+    if (n < 1) {
+        return 0;
     }
 
-    return sum;
+    else {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += array[i];
+        }
+
+        return sum;
+    }
 }

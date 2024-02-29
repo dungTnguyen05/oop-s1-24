41438a291b235969001c383365937fd92c6425ee
dungTnguyen05 <tridung.190705@gmail.com>
2024-02-29 11:21:29 +1030

    function-1-3.cpp

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,11 +1,17 @@
 int num_count(int array[], int n, int number) {
-    int count = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (array[i] == number) {
-            count++;
-        }
+    if (n < 1) {
+        return 0;
     }
 
-    return count;
+    else {
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] == number) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }

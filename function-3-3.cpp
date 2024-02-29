6636b3405722d7a3c14ea1110ac92d3543ad6b49
dungTnguyen05 <tridung.190705@gmail.com>
2024-02-29 10:31:04 +1030

    function-3-3.cpp

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -1,3 +1,17 @@
 double weighted_average(int array[], int n) {
-    
+    double weighted_sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (array[j] == array[i]) {
+                count++;
+            }
+        }
+
+        weighted_sum += array[i] * count;
+        count = 0;
+    }
+
+    return weighted_sum/n;
 }

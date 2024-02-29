4a140e532ecdacbeab637a78a0c7e1fc45dd97f2
dungTnguyen05 <tridung.190705@gmail.com>
2024-02-29 10:41:59 +1030

    main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,15 @@ using namespace std;
 extern int array_sum(int array[], int n);
 
 int main() {
-    int array[6] = {1, 9, 7, 7, 1, 1};
-    int n = 6;
-    cout << array_sum(array, n);
+    int a[6] = {1, 9, 7, 7, 1, 1};
+    int x = 6;
+    cout << array_sum(a, x) << endl;
+
+    int b[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int y = 8;
+    cout << array_sum(b, y) << endl;
+
+    int c[0] = {};
+    int z = 0;
+    cout << array_sum(c, z) << endl;
 }

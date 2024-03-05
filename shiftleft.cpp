d29d99e06e93b0b031e4b3149b2bdaf80f1d92da
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-05 12:56:40 +1030

    .

---
+++ shiftleft.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+string shiftleft(string binary) {
+    string shifted = binary;
+
+    for (int i = binary.length() - 1; i >= 0; i--) {
+        shifted[i - 1] = binary[i];
+    }
+
+    shifted[binary.length() - 1] = '0';
+
+    return shifted;
+}
+
+int main() {
+    cout << shiftleft("000001") << endl;
+}

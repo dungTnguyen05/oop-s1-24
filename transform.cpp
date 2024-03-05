3b4bdb46da6d7ad010f10cd27baf50a435806e7c
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-05 13:36:49 +1030

    .

--- transform.cpp
+++ transform.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void dec_to_bin(int decimal) {
+    int power_2 = 1;
+
+    while (decimal > power_2) {
+        power_2 *= 2;
+    }
+
+    power_2 /= 2;
+
+    while (decimal != 0) {
+        cout << decimal/power_2;
+        decimal = decimal % power_2;
+        power_2 /= 2;
+    }
+
+    cout << endl;
+}
+
 int main() {
-    
+    int ntd = 197;
+    dec_to_bin(ntd);
+
+    int nmc = 35;
+    dec_to_bin(nmc);
+
+    int tnt = 279;
+    dec_to_bin(tnt);
 }

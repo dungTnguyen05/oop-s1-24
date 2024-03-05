0aecc3c19db6a1b8061285b3776f0eb5d2670ad6
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-06 02:40:28 +1030

    .

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,3 +1,35 @@
 int sum_if_palindrome(int integers[], int length) {
-    
+    if (is_palindrome(integers, length) == true) {
+        return sum_array_elements(integers, length);
+    }
+
+    return -2;
+}
+
+bool is_palindrome(int integers[], int length) {
+    if (length < 1) {
+        return false;
+    }
+
+    for (int i = 0; i < length/2; i++) {
+        if (integers[i] != integers[length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int sum_array_elements(int integers[], int length) {
+    if (length < 1) {
+        return -1;
+    }
+
+    int res = 0;
+
+    for (int i = 0; i < length; i++) {
+        res += integers[i];
+    }
+
+    return res;
 }

2af326ac7cb6b997168a2a96f0b673cc0afc4beb
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-06 02:48:24 +1030

    .

---
+++ function-2-4.cpp
@@ -0,0 +1,39 @@
+int sum_min_max(int integers[], int length) {
+    if (length < 1) {
+        return -1;
+    }
+
+    return array_min(integers, length) + array_max(integers, length);
+}
+
+int array_min(int integers[], int length) {
+    if (length < 1) {
+        return -1;
+    }
+
+    int res = integers[0];
+
+    for (int i = 1; i < length; i++) {
+        if (integers[i] < res) {
+            res = integers[i];
+        }
+    }
+
+    return res;
+}
+
+int array_max(int integers[], int length) {
+    if (length < 1) {
+        return -1;
+    }
+
+    int res = integers[0];
+
+    for (int i = 1; i < length; i++) {
+        if (integers[i] > res) {
+            res = integers[i];
+        }
+    }
+
+    return res;
+}

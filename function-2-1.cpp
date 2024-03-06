d39e7f86668e1bddc4e69d9e5b7369613da8ae44
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-07 00:56:24 +1030

    .

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 void print_binary_str(std::string decimal_number) {
     int base10 = stoi(decimal_number);
-    
     int power2 = 1;
     int len = 0;
 
@@ -12,14 +11,19 @@ void print_binary_str(std::string decimal_number) {
         len++;
     }
 
+    power2 /= 2;
+
     string binary_number[len];
     int digit;
 
-    for (int i = len - 1; i >= 0; i--) {
-        digit = base10 % 2;
-        binary_number[len - 1 - i] = digit;
-        base10 = (base10 - digit)/2;
+    for (int i = 0; i < len; i++) {
+        int digit = base10/power2;
+        binary_number[i] = char(digit + 48); // ASCII table (store binary number in string type)
+        base10 = base10 % power2;
+        power2 /= 2;
+
+        cout << binary_number[i];
     }
 
-    cout << binary_number;
+    cout << endl;
 }

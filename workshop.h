b17e7ce63cbed6c4fef741ddf445dde4a022d11e
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-12 10:33:15 +1030

    .

---
+++ workshop.h
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void changeValue(double *x) {
+    *x = 42;
+}
+
+void printArray(double *x, int len) {
+    for (int i = 0; i < len; i++) {
+        cout << *(x + i) << " ";
+    }
+}
+
+double arrayMax(double *x, int len) {
+    double res = x[0];
+
+    for (int i = 1; i < len; i++) {
+        if (x[i] > res) {
+            res = x[i];
+        }
+    }
+
+    return res;
+}
+
+double *dynamicArray(int len, double num) {
+    double *x = new double[len];
+
+    for (int i = 0; i < len; i++) {
+        x[i] = num;
+    }
+
+    return x;
+}

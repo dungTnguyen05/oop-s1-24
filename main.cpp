a644e1e8302cfe564fe46cbce6b3dca4ae9d3134
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-12 10:32:58 +1030

    .

--- main.cpp
+++ main.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
 #include "workshop.h"
+using namespace std;
+
+int main() {
+    double a[6] = {1, 9, 7, 7, 1, 1};
+
+    changeValue(a + 5);
+    cout << a[5] << endl;
+
+    printArray(a, 6);
+    cout << endl;
+
+    cout << arrayMax(a, 6);
+    cout << endl;
+
+    double *x = dynamicArray(6, 8);
+
+    for (int i = 0; i < 6; i++) {
+        cout << x[i] << " ";
+    }
+
+    cout << endl;
+
+    double b[10] = {3, 1, 4, 1, 5, 2, 7, 1, 8, 2};
+
+    changeValue(&b[6]);
+    cout << b[6] << endl;
+
+    printArray(b, 10);
+    cout << endl;
+
+    cout << arrayMax(b, 10);
+    cout << endl;
+
+    double *y = dynamicArray(10, 10);
+
+    for (int i = 0; i < 10; i++) {
+        cout << y[i] << " ";
+    }
+
+    cout << endl;
+}

c6f1800d41cf2233c1af6d07f812d4e5f1e2f888
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-19 01:56:59 +1030

    .

---
+++ function-3-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int *readNumbers() {
+    int *arr = new int[10];
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Enter a number: ";
+        cin >> arr[i];
+    }
+
+    return arr;
+}
+
+bool equalsArray(int *numbers1, int *numbers2, int length) {
+    if (length < 1) {
+        return false;
+    }
+
+    for (int i = 0; i < length; i++) {
+        if (numbers1[i] != numbers2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}

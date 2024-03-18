652e24604fa4b4e3f5a6a34fbee5ccfbb28cee3c
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-19 00:51:55 +1030

    .

---
+++ main-1-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+extern int *readNumbers();
+extern void printNumbers(int *numbers, int length);
+
+int main() {
+    int *arr = readNumbers();
+    int len = 10;
+    printNumbers(arr, len);
+}

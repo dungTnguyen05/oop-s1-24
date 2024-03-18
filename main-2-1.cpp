3b7f58bb83332fa2183cc978e8faf4d4df47a6e6
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-19 01:38:23 +1030

    .

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 using namespace std;
 
+extern int *readNumbers();
 extern void hexDigits(int *numbers, int length);
 
 int main() {
-    int a[10] = {1, 9, 0, 7, 0, 7, 1, 1, 15, 9};
-    int x = 10;
-    hexDigits(a, x);
-
-    int b[5] = {10, 13, 11, 10, 12};
-    int y = 5;
-    hexDigits(b, y);
-
-    int c[12] = {1, 10, 2, 11, 3, 12, 4, 13, 5, 14, 6, 15};
-    int z = 12;
-    hexDigits(c, z);
+    int *a = readNumbers();
+    int len = 10;
+    hexDigits(a, len);
+    delete[] a;
 }

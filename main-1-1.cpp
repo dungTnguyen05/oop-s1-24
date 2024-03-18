baacc3004012b305480c9781c3783038f6a00f7a
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-19 09:45:37 +1030

    .

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,10 +5,21 @@ using namespace std;
 extern Person *createPersonArray(int n);
 
 int main() {
-    int a = 5;
+    int a = 6;
     Person *x = createPersonArray(a);
 
     for (int i = 0; i < a; i++) {
         cout << x[i].name << ", " << x[i].age << endl;
     }
+
+    delete[] x;
+
+    int b = 8;
+    Person *y = createPersonArray(b);
+
+    for (int i = 0; i < b; i++) {
+        cout << y[i].name << ", " << y[i].age << endl;
+    }
+
+    delete[] y;
 }

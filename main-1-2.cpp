188645fce86f233fa0cf9f7cba30e029fae7d20f
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-19 20:51:56 +1030

    .

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include "Person.h"
 using namespace std;
+
+extern PersonList createPersonList(int n);
+
+int main() {
+    int a = 6;
+    PersonList x = createPersonList(a);
+    cout << x.numPeople << endl;
+
+    for (int i = 0; i < a; i++) {
+        cout << x.people[i].name << ", " << x.people[i].age << endl;
+    }
+
+    int b = 8;
+    PersonList y = createPersonList(b);
+    cout << y.numPeople << endl;
+
+    for (int i = 0; i < b; i++) {
+        cout << y.people[i].name << ", " << y.people[i].age << endl;
+    }
+}

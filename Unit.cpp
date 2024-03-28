64c98e75ac1e0c500170eb146069b0f75757956f
dungTnguyen05 <tridung.190705@gmail.com>
2024-03-28 10:57:29 +1030

    .

--- Unit.cpp
+++ Unit.cpp
@@ -4,6 +4,18 @@ Unit::Unit(): Unit(0, 0, 0) {
 }
 
 Unit::Unit(int unit_val, int num_beds, double unit_size) {
+    if (unit_val < 0) {
+        value = 0;
+    }
+
+    if (num_beds < 0) {
+        bedrooms = 0;
+    }
+
+    if (unit_size < 0) {
+        size = 0;
+    }
+    
     value = unit_val;
     bedrooms = num_beds;
     size = unit_size;

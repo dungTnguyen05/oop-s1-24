77965f37b54f868929e758d95a695146ed66f275
dungTnguyen05 <tridung.190705@gmail.com>
2024-04-04 12:03:40 +1030

    .

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include "ParkingLot.h"
+#include "Vehicle.h"
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
 using namespace std;
 
 int main() {
-    
+    ParkingLot a(6);
+    string vehicle_type;
+    int new_ID;
+
+    for (int i = 0; i < 6; i++) {
+        cout << "Type of vehicle: ";
+        cin >> vehicle_type;
+
+        if (vehicle_type == "Car") {
+            cout << "Enter the ID: ";
+            cin >> new_ID;
+            a.parkVehicle(new Car(new_ID));
+        }
+
+        else if (vehicle_type == "Bus") {
+            cout << "Enter the ID: ";
+            cin >> new_ID;
+            a.parkVehicle(new Bus(new_ID));
+        }
+
+        else {
+            cout << "Enter the ID: ";
+            cin >> new_ID;
+            a.parkVehicle(new Motorbike(new_ID));
+        }
+
+        cout << "current: " << a.getCount() << endl;
+    }
 }

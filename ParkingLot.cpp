8bff881d4e7c23c8b98e9d8083b5800e0f8efa61
dungTnguyen05 <tridung.190705@gmail.com>
2024-04-04 11:44:53 +1030

    .

---
+++ ParkingLot.cpp
@@ -0,0 +1,48 @@
+#include "ParkingLot.h"
+
+ParkingLot::ParkingLot(int new_capacity) {
+    capacity = new_capacity;
+    current = 0;
+    vehicles = new Vehicle *[capacity];
+}
+
+void ParkingLot::parkVehicle(Vehicle *new_vehicle) {
+    if (current < capacity) {
+        vehicles[current] = new_vehicle;
+        current++;
+    }
+
+    else {
+        cout << "The lot is full" << endl;
+    }
+}
+
+void ParkingLot::unparkVehicle(int ID_removed) {
+    int count = 0;
+
+    for (int i = 0; i < current; i++) {
+        if (ID_removed == vehicles[i] -> getID()) {
+            delete[] vehicles[i];
+
+            for (int j = i; j < current - 1; j++) {
+                vehicles[j] = vehicles[j + 1];  
+            }
+            
+            current--;
+            break;
+        }
+
+        count++;
+    }
+
+    if (count == current) {
+        cout << "Vehicle not in the lot" << endl;
+    }
+
+    cout << endl;
+}
+
+int ParkingLot::getCount() {
+    return current;
+}
+
---
+++ ParkingLot.h
@@ -0,0 +1,20 @@
+#ifndef PARKINGLOT_H
+#define PARKINGLOT_H
+
+#include <iostream>
+#include "Vehicle.h"
+using namespace std;
+
+class ParkingLot {
+    private:
+        int capacity;
+        int current;
+        Vehicle **vehicles;
+    public:
+        ParkingLot(int new_capacity);
+        void parkVehicle(Vehicle *new_vehicle);
+        void unparkVehicle(int ID_removed);
+        int getCount();
+};
+
+#endif
